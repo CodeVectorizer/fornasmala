04ee141c2f59fbf90e600c35631edb2cf8428f45
MrHafid <achmadhafid668@gmail.com>
2023-04-05 23:06:22 +0700

    add view daftar kampus and detail kampus but data is demo

---
+++ lib/pages/campus/list_campus.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fornasmala/pages/campus/detail_campus.dart';
+import 'package:fornasmala/widgets/search_box.dart';
+
+class ListCampusPage extends StatefulWidget {
+  const ListCampusPage({super.key});
+
+  @override
+  State<ListCampusPage> createState() => _ListCampusPageState();
+}
+
+class _ListCampusPageState extends State<ListCampusPage> {
+  List listCampus = [
+    {
+      'logo': 'assets/images/logo_polije.png',
+      'title': 'POLITEKNIK NEGERI JEMBER',
+      'phone1': '+62 331 333533',
+      'phone2': '+62 331 333531',
+      'email': 'politeknik@polije.ac.id',
+      'province': 'Jawa Timur',
+      'camp1': {
+        'domisili': 'Jember',
+        'alamat': 'Jl. Mastrip PO BOX 164, Jember - Jawa Timur- Indonesia'
+      },
+      'camp2': {
+        'domisili': 'Bondowoso',
+        'alamat': 'Jl. KH. Wahid Hasyim No 41, Bondowoso - Jawa Timur'
+      },
+      'camp3': {
+        'domisili': 'Nganjuk',
+        'alamat': 'Jl. Gatot Subroto No 02, Nganjuk - Jawa Timur'
+      },
+      'camp4': {
+        'domisili': 'Sidoarjo',
+        'alamat':
+            'Jl. Kutuk Barat gg. Sekolahan No 1 Sidokare, Sidoarjo - Jawa Timur'
+      },
+      'jurusan': [
+        {'no': 1, 'name': 'Produksi Pertanian'},
+        {'no': 1, 'name': 'Teknologi Pertanian'},
+        {'no': 1, 'name': 'Peternakan'},
+        {'no': 1, 'name': 'Manajemen Agribisnis'},
+        {'no': 1, 'name': 'Teknologi Informasi'},
+        {'no': 1, 'name': 'Bahasa, Komunikasi & Pariwisata'},
+        {'no': 1, 'name': 'Kesehatan'},
+        {'no': 1, 'name': 'Teknik'},
+      ],
+      'website': 'https://polije.ac.id/'
+    },
+    {
+      'logo': 'assets/images/logo_unej.png',
+      'title': 'UNIVERSITAS NEGERI JEMBER',
+      'province': 'Jawa Timur',
+      'phone1': '+62 331 333533',
+      'phone2': '+62 331 333531',
+      'email': 'politeknik@polije.ac.id',
+      'camp1': {
+        'domisili': 'Jember',
+        'alamat': 'Jl. Mastrip PO BOX 164, Jember - Jawa Timur- Indonesia'
+      },
+      'camp2': {
+        'domisili': 'Bondowoso',
+        'alamat': 'Jl. KH. Wahid Hasyim No 41, Bondowoso - Jawa Timur'
+      },
+      'camp3': {
+        'domisili': 'Nganjuk',
+        'alamat': 'Jl. Gatot Subroto No 02, Nganjuk - Jawa Timur'
+      },
+      'camp4': {
+        'domisili': 'Sidoarjo',
+        'alamat':
+            'Jl. Kutuk Barat gg. Sekolahan No 1 Sidokare, Sidoarjo - Jawa Timur'
+      },
+      'jurusan': [
+        {'no': 1, 'name': 'Produksi Pertanian'},
+        {'no': 1, 'name': 'Teknologi Pertanian'},
+        {'no': 1, 'name': 'Peternakan'},
+        {'no': 1, 'name': 'Manajemen Agribisnis'},
+        {'no': 1, 'name': 'Teknologi Informasi'},
+        {'no': 1, 'name': 'Bahasa, Komunikasi & Pariwisata'},
+        {'no': 1, 'name': 'Kesehatan'},
+        {'no': 1, 'name': 'Teknik'},
+      ],
+      'website': 'https://polije.ac.id/'
+    },
+    {
+      'logo': 'assets/images/logo_unej.png',
+      'title': 'UNIVERSITAS NEGERI JEMBER',
+      'phone1': '+62 331 333533',
+      'phone2': '+62 331 333531',
+      'email': 'politeknik@polije.ac.id',
+      'province': 'Jawa Timur',
+      'camp1': {
+        'domisili': 'Jember',
+        'alamat': 'Jl. Mastrip PO BOX 164, Jember - Jawa Timur- Indonesia'
+      },
+      'camp2': {
+        'domisili': 'Bondowoso',
+        'alamat': 'Jl. KH. Wahid Hasyim No 41, Bondowoso - Jawa Timur'
+      },
+      'camp3': {
+        'domisili': 'Nganjuk',
+        'alamat': 'Jl. Gatot Subroto No 02, Nganjuk - Jawa Timur'
+      },
+      'camp4': {
+        'domisili': 'Sidoarjo',
+        'alamat':
+            'Jl. Kutuk Barat gg. Sekolahan No 1 Sidokare, Sidoarjo - Jawa Timur'
+      },
+      'jurusan': [
+        {'no': 1, 'name': 'Produksi Pertanian'},
+        {'no': 1, 'name': 'Teknologi Pertanian'},
+        {'no': 1, 'name': 'Peternakan'},
+        {'no': 1, 'name': 'Manajemen Agribisnis'},
+        {'no': 1, 'name': 'Teknologi Informasi'},
+        {'no': 1, 'name': 'Bahasa, Komunikasi & Pariwisata'},
+        {'no': 1, 'name': 'Kesehatan'},
+        {'no': 1, 'name': 'Teknik'},
+      ],
+      'website': 'https://polije.ac.id/'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: listCampus
+                  .asMap()
+                  .map((i, e) => MapEntry(
+                      i,
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.of(context).push(CupertinoPageRoute(
+                              builder: (context) => DetailCampusPage(
+                                    dataCampus: e,
+                                  )));
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: i == 0
+                              ? const EdgeInsets.only(
+                                  left: 14, right: 14, bottom: 14, top: 240)
+                              : const EdgeInsets.all(14),
+                          padding: const EdgeInsets.all(12),
+                          decoration: BoxDecoration(
+                              color: Colors.grey.shade400,
+                              borderRadius: BorderRadius.circular(18)),
+                          child: Column(
+                            children: [
+                              Image.asset(
+                                e['logo'],
+                                width: 150,
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              Text(
+                                '${e['title']}'.toUpperCase(),
+                                style: const TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 17),
+                              ),
+                              Text(
+                                e['province'],
+                                style: const TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.normal,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      )))
+                  .values
+                  .toList(),
+            ),
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 220,
+            color: Colors.lightBlue,
+            padding: const EdgeInsets.all(14),
+            child: Column(
+              children: const [
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'DAFTAR KAMPUS',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text('berikut daftar kampus ternama yang ada di Indonesia',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 10,
+                ),
+                SearchBox(
+                  color: Colors.white30,
+                  textColor: Colors.white,
+                  valueColor: Colors.white,
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:fornasmala/pages/campus/list_campus.dart';
 import 'package:fornasmala/theme.dart';
 
 class Home extends StatefulWidget {
@@ -16,27 +18,40 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlue,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 0,
         actions: [
-          Icon(Icons.notifications),
-          Icon(Icons.location_pin),
-          new Center(
-              child: Text(
-            'Lamongan',
-            textAlign: TextAlign.center,
-            style: new TextStyle(
-              fontSize: 16.0,
-              color: Colors.white,
-            ),
-          )),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.notifications),
+            padding: EdgeInsets.zero,
+          ),
+          TextButton(
+              onPressed: () {},
+              child: Row(
+                children: const [
+                  Icon(
+                    Icons.location_pin,
+                    color: Colors.white,
+                  ),
+                  Center(
+                      child: Text(
+                    'Lamongan',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.white,
+                    ),
+                  )),
+                ],
+              )),
         ],
         backgroundColor: Colors.lightBlue,
       ),
       body: SingleChildScrollView(
         child: SafeArea(
-          child: Column(
+          child: Stack(
             children: [
               Container(
                 // decoration: BoxDecoration(
@@ -47,31 +62,33 @@ class _HomeState extends State<Home> {
                 //   fit: BoxFit.cover,
                 // )
                 // ),
-                padding: EdgeInsets.symmetric(horizontal: 20),
+                color: Colors.lightBlue,
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                height: 300,
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Container(
-                      margin: EdgeInsets.only(top: 50),
-                      child: Text(
+                      // margin: const EdgeInsets.only(top: 25),
+                      child: const Text(
                         "Hello Rudy",
                         style: TextStyle(color: Colors.white, fontSize: 20),
                       ),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 10,
                     ),
-                    Text(
+                    const Text(
                       "Please find a source you best to use",
                       style: TextStyle(color: Colors.white, fontSize: 14),
                     ),
-                    SizedBox(
-                      height: 20,
+                    const SizedBox(
+                      height: 10,
                     ),
                     Container(
-                      margin: EdgeInsets.only(top: 10),
-                      padding: EdgeInsets.all(10),
+                      margin: const EdgeInsets.only(top: 10),
+                      padding: const EdgeInsets.all(10),
                       decoration: BoxDecoration(
                           borderRadius: BorderRadius.circular(10),
                           color: Colors.white.withOpacity(0.2)),
@@ -92,7 +109,7 @@ class _HomeState extends State<Home> {
                             )
                           ]),
                     ),
-                    SizedBox(
+                    const SizedBox(
                       height: 20,
                     ),
                   ],
@@ -100,12 +117,12 @@ class _HomeState extends State<Home> {
               ),
               Container(
                 width: MediaQuery.of(context).size.width,
-                height: 485,
-                margin: EdgeInsets.only(top: 10),
-                padding: EdgeInsets.symmetric(
+                // height: 485,
+                margin: const EdgeInsets.only(top: 150),
+                padding: const EdgeInsets.symmetric(
                   horizontal: 25,
                 ),
-                decoration: BoxDecoration(
+                decoration: const BoxDecoration(
                     borderRadius: BorderRadius.vertical(
                       top: Radius.circular(20),
                     ),
@@ -113,7 +130,7 @@ class _HomeState extends State<Home> {
                 child: Column(
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: [
-                      SizedBox(
+                      const SizedBox(
                         height: 20,
                       ),
                       Row(
@@ -137,40 +154,42 @@ class _HomeState extends State<Home> {
                       ),
                       Container(
                         height: 70,
-                        margin: EdgeInsets.only(top: 20),
-                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        margin: const EdgeInsets.only(top: 20),
+                        padding: const EdgeInsets.symmetric(horizontal: 14),
                         decoration: BoxDecoration(
                             borderRadius: BorderRadius.circular(10),
                             color: Colors.lightBlue),
                         child: Row(
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  "PULANG",
-                                  style: TextStyle(
-                                      color: Colors.yellow,
-                                      fontSize: 16,
-                                      fontWeight: semiBold),
-                                ),
-                                Text(
-                                  "Pemuda Untuk Lamongan",
-                                  style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 14,
-                                      fontWeight: semiBold),
-                                ),
-                              ],
+                            Expanded(
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "PULANG",
+                                    style: TextStyle(
+                                        color: Colors.yellow,
+                                        fontSize: 16,
+                                        fontWeight: semiBold),
+                                  ),
+                                  Text(
+                                    "Pemuda Untuk Lamongan",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 13,
+                                        fontWeight: semiBold),
+                                  ),
+                                ],
+                              ),
                             ),
                             Image.asset('assets/images/lamongan.png',
                                 height: 50, width: 110, fit: BoxFit.fill),
                           ],
                         ),
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 30,
                       ),
                       Row(
@@ -183,10 +202,10 @@ class _HomeState extends State<Home> {
                                 height: 100,
                                 width: 100,
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 height: 10,
                               ),
-                              Text('Kos & Kontrakan')
+                              const Text('Kos & Kontrakan')
                             ],
                           ),
                           Column(
@@ -196,10 +215,10 @@ class _HomeState extends State<Home> {
                                 height: 100,
                                 width: 100,
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 height: 10,
                               ),
-                              Text('Food')
+                              const Text('Food')
                             ],
                           ),
                           Column(
@@ -209,32 +228,38 @@ class _HomeState extends State<Home> {
                                 height: 100,
                                 width: 100,
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 height: 10,
                               ),
-                              Text('Transportation')
+                              const Text('Transportation')
                             ],
                           ),
                         ],
                       ),
-                      SizedBox(
+                      const SizedBox(
                         height: 30,
                       ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Column(
-                            children: [
-                              Image.asset(
-                                'assets/images/kampus.png',
-                                height: 100,
-                                width: 100,
-                              ),
-                              SizedBox(
-                                height: 10,
-                              ),
-                              Text('Campus')
-                            ],
+                          GestureDetector(
+                            onTap: () => Navigator.of(context).push(
+                                CupertinoPageRoute(
+                                    builder: (context) =>
+                                        const ListCampusPage())),
+                            child: Column(
+                              children: [
+                                Image.asset(
+                                  'assets/images/kampus.png',
+                                  height: 100,
+                                  width: 100,
+                                ),
+                                const SizedBox(
+                                  height: 10,
+                                ),
+                                const Text('Campus')
+                              ],
+                            ),
                           ),
                           Column(
                             children: [
@@ -243,10 +268,10 @@ class _HomeState extends State<Home> {
                                 height: 100,
                                 width: 100,
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 height: 10,
                               ),
-                              Text('Organizer')
+                              const Text('Organizer')
                             ],
                           ),
                           Column(
@@ -256,10 +281,10 @@ class _HomeState extends State<Home> {
                                 height: 100,
                                 width: 100,
                               ),
-                              SizedBox(
+                              const SizedBox(
                                 height: 10,
                               ),
-                              Text('Activity')
+                              const Text('Activity')
                             ],
                           ),
                         ],
